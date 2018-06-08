ebe1e01f9355636d9431ce234396723c996824e6
Himashi Hettege Dona <himashi99@hotmail.com>
2018-06-07 22:07:39 -0400

    Cleaned up scripts

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,60 +1,55 @@
 //if name entered <3 characters, HTML5 validation error sent
-var userName = document.getElementById("name");
+var userName = document.getElementById('name');
 
-userName.addEventListener("input", function (event) {
+userName.addEventListener('input', function (event) {
   if (userName.validity.tooShort) {
-    userName.setCustomValidity("Please enter a valid name");
+    userName.setCustomValidity('Please enter a valid name');
   } else {
-    userName.setCustomValidity("");
+    userName.setCustomValidity('');
   }
 });
 
 //if email entered is not formatted correctly, HTML5 validation error sent
-var mail = document.getElementById("email");
+var mail = document.getElementById('email');
 
-mail.addEventListener("input", function (event) {
+mail.addEventListener('input', function (event) {
   if (mail.validity.typeMismatch) {
-    mail.setCustomValidity("Please enter a valid email");
+    mail.setCustomValidity('Please enter a valid email');
   } else {
-    mail.setCustomValidity("");
+    mail.setCustomValidity('');
   }
 });
 
 //if age is less than 14, HTML validation error sent
-var ageLimit = document.getElementById("number");
+var ageLimit = document.getElementById('number');
 
-ageLimit.addEventListener("input", function (event) {
+ageLimit.addEventListener('input', function (event) {
   if (ageLimit.validity.rangeUnderflow) {
-    ageLimit.setCustomValidity("You must be working age 14+");
+    ageLimit.setCustomValidity('You must be working age 14+');
   } else {
-    ageLimit.setCustomValidity("");
+    ageLimit.setCustomValidity('');
   }
 });
 
 //If no movies are selected, error message is displayed and form won't submit
-$("form").submit(function(e){
+$('form').submit(function(e){
+  e.preventDefault();
   if ($('input:checkbox').filter(':checked').length < 1) {
-      $('.error-message').css('display','block');
-      console.log('morbo!');
-      e.preventDefault();
+    $('.error-message').css('display','block');
   } else {
-    //open up new window with message
-    window.open("page2.html");
-    // $(".dialog-box").css({ display: "none" });
-    // alert("Congrats");
+    // show complete page
+    window.location = 'page2.html';
   }
 });
 
-//If user clicks on the last movie selection i.e. None of the Above, all other choices are disabled
-$(function() {
-  disable_movie();
-  $("#movie-checkbox10").click(disable_movie);
-});
+// Attach an onClick listener to last check-box becuase they have no soul
+// those bastards, they can go fuck themselves!! 💩
+$('#no-soul').click(disableAllMovies);
 
-function disable_movie() {
+function disableAllMovies() {
   if (this.checked) {
-    $(".disabled-group").attr("disabled", true);
+    $('.disabled-group').attr('disabled', true);
   } else {
-    $(".disabled-group").removeAttr("disabled");
+    $('.disabled-group').removeAttr('disabled');
   }
 }
